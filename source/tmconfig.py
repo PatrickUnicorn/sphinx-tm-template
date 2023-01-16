424180cb4d98c2bc261ee526f04b8257aa57e6cf
PatrickUnicorn <patrick.alves03@hotmail.com>
2023-01-16 06:17:07 +0000

    petits changement

--- source/tmconfig.py
+++ source/tmconfig.py
@@ -1,15 +1,15 @@
 class TMConfig:
 
-    title = u'Titre du TM'
-    first_name = 'Prénom'
-    last_name = 'Nom'
+    title = u'Création d’un composant web capable de générer des quiz et autres exercices éducatifs '
+    first_name = 'Patrick'
+    last_name = 'Oliveira Alves'
     author = f'{first_name} {last_name}'
     year = u'2022'
     month = u'Décembre'
     seminary_title = u'Développement Web'
     tutor = u"Cédric Donner et Johan Jobin"
     release = "Version intermédiaire"
-    repository_url = "https://github.com/{your-docs-url}"
+    repository_url = "https://github.com/PatrickUnicorn/sphinx-tm-template"
 
     @classmethod
     def date(cls):
